e983cef2992ee1295550c6e240d64c8fcabb37ad
luu The Manh <62170133+luuthemanhgch17279@users.noreply.github.com>
2020-06-11 23:39:37 +0700

    SECTiON 1 AND EXCERCISE DAY 1

---
+++ EXCERCISE_DAY_1/index.js
@@ -0,0 +1,38 @@
+console.log('Excercise Day 1');
+console.log('================');
+console.log('Bai 1: ---------');
+
+// let a =  prompt('Please enter your name: ');
+// let b = prompt('Please enter your age: ');
+
+// console.log(a + ' is ' + b + ' years old!');
+
+console.log('Bai 2: ---------');
+// for(let i = 0; i < 5; i ++){
+//     let a =  prompt('Please enter your name: ');
+//     let b = prompt('Please enter your age: ');
+//     console.log(a + ' is ' + b + ' years old!');
+// }
+
+console.log('Bai Tap Loop: ==');
+console.log('Bai 1: ---------');
+console.log('Day so tu 0 - 100');
+// for (let i = 0; i <=100; i ++){
+//     console.log('Number ' + i);
+// }
+
+console.log('Bai 2: ---------');
+console.log('Day so tu 100 - 1');
+// for (let i = 100; i >= 1; i --){
+//     console.log('Number ' + i);
+// }
+
+
+console.log('Bai 3: ---------');
+console.log('Cac so chia het cho 5 tu 5 - 70');
+// for(let i = 5; i <= 70; i ++){
+//     if (i%5 == 0){
+//         console.log('Number: ' + i);
+//     }
+// }
+
---
+++ SECTION1/index.js
@@ -0,0 +1,39 @@
+// let x = 5;
+// x = 6;
+// s = 'Hi there';
+
+// console.log(x);
+// console.log(s);
+
+// prompt('Question');
+
+// let k = prompt('kkkkk');
+// console.log(k);
+
+
+// let a =10;
+// let c = 20;
+// {
+//     console.log(a);
+//     console.log(a+c);
+//     console.log(a-c);
+//     console.log(a/c);
+//     console.log(a*c);
+//     console.log(a%c);
+// }
+
+// {
+//     let b = 10;
+// }
+// console.log(b);
+
+// let x = 6;
+// x+=7
+// console.log(x);
+
+let x = 'Hello anh em';
+// console.log(x.toLocaleUpperCase());
+let y = x.toUpperCase();
+console.log(y);
+let z = x.toLowerCase();
+console.log(z);
