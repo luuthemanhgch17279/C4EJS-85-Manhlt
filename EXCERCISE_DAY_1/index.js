d3b8b20688734a07dace8460137c32a4aa2977d8
luu The Manh <62170133+luuthemanhgch17279@users.noreply.github.com>
2020-06-18 21:10:55 +0700

    9:10

--- EXCERCISE_DAY_1/index.js
+++ EXCERCISE_DAY_1/index.js
@@ -1,6 +1,6 @@
-console.log('Excercise Day 1');
-console.log('================');
-console.log('Bai 1: ---------');
+// console.log('Excercise Day 1');
+// console.log('================');
+// console.log('Bai 1: ---------');
 
 // let a =  prompt('Please enter your name: ');
 // let b = prompt('Please enter your age: ');
@@ -8,28 +8,32 @@ console.log('Bai 1: ---------');
 // console.log(a + ' is ' + b + ' years old!');
 
 console.log('Bai 2: ---------');
-// for(let i = 0; i < 5; i ++){
-//     let a =  prompt('Please enter your name: ');
-//     let b = prompt('Please enter your age: ');
-//     console.log(a + ' is ' + b + ' years old!');
-// }
-
-console.log('Bai Tap Loop: ==');
-console.log('Bai 1: ---------');
-console.log('Day so tu 0 - 100');
+// Su Dung Vong Lap For
+// console.log('Bai Tap Loop: ==');
+// console.log('Bai 1: ---------');
+// console.log('Day so tu 0 - 100');
 // for (let i = 0; i <=100; i ++){
 //     console.log('Number ' + i);
 // }
 
-console.log('Bai 2: ---------');
-console.log('Day so tu 100 - 1');
+//Su Dung Vong Lap While
+let x = 0;
+let k = prompt("Enter your number:");
+while(x < k){
+    x++;
+    console.log('Number: ' + x);
+}
+
+
+// console.log('Bai 2: ---------');
+// console.log('Day so tu 100 - 1');
 // for (let i = 100; i >= 1; i --){
 //     console.log('Number ' + i);
 // }
 
 
-console.log('Bai 3: ---------');
-console.log('Cac so chia het cho 5 tu 5 - 70');
+// console.log('Bai 3: ---------');
+// console.log('Cac so chia het cho 5 tu 5 - 70');
 // for(let i = 5; i <= 70; i ++){
 //     if (i%5 == 0){
 //         console.log('Number: ' + i);
---
+++ SECTION2/index.js
@@ -0,0 +1,18 @@
+// let n = 5;
+// console.log(`I  have ${n} dogs`);
+
+// let m = Number('45');
+// console.log(`Ket Qua Lan 1: ${n + m}`);
+
+// let k = String(198);
+// console.log(`Ket Qua Lan 2: ${k+m+n}`);
+
+// let x = 0;
+// while (x < 3) {
+//     x++;
+//     console.log(x);
+// }
+
+for(let x = 0; x <= 3; x++){
+    console.log(x);
+}
