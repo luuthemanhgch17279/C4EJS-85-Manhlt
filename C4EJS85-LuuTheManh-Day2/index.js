fed1d94d9ca5c7a8c51270c5638e0aba8824edc5
Manhlt <luuthemanhgch17279@gmail.com>
2020-06-22 20:09:49 +0700

    11

--- C4EJS85-LuuTheManh-Day2/index.js
+++ C4EJS85-LuuTheManh-Day2/index.js
@@ -93,7 +93,7 @@
 // }
 // alert("The factorial of " + n + " is " + a);
 
-// console.log("Bài Tập 5: Write a program asking users their age, and then decide if they are old enough to view a 14+ content")
+// console.log("Bài Tập 5: Write a program asking users their age, and then decide if they are old enough to view a 14+ content");
 // let ageUsers = prompt("Enter your age: ");
 // if(ageUsers >= 14){
 //   alert("Enjoy!!!!!!");
