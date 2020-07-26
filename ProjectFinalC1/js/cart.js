33ef5935680a23f87899f4c3a3bbbaaa7368a3fe
luu The Manh <62170133+luuthemanhgch17279@users.noreply.github.com>
2020-07-26 14:47:47 +0700

    2:47pm

--- ProjectFinalC1/js/cart.js
+++ ProjectFinalC1/js/cart.js
@@ -6,102 +6,156 @@ function loadNumberInCart() {
         document.getElementById('cartNumbers').textContent = productNumberInCarts;
     }
 }
+
 //function này gọi ra những product đã đc chọn
 function loadListCartProduct() {
     let cartContent = document.getElementById('content');
     let cartTotal = localStorage.getItem('totalCart');
-    let cartItems = localStorage.getItem('productInCart');
-    cartItems = JSON.parse(cartItems);
-    cartItems = (Object.values(cartItems));
-    //console.log(cartItems[1]);
-    if (cartItems && cartContent) {
+    let productInCart = localStorage.getItem('productInCart');
+    productInCart = JSON.parse(productInCart);
+    productInCart = (Object.values(productInCart));
+    if (productInCart && cartContent) {
         cartContent.innnerHTML = '';
-        Object.values(cartItems).map(item => {
-            cartContent.innerHTML += `
-        <div class="listCartProduct">
-            <img src="" alt="">
-          Name Product:  <span>${item.productName}</span>
-        </div>
-        <div class="productPrice">
-          Price: <span> ${item.price}</span>
-        </div>
-        <div claas="productQuantity">
-        Product Quantity:
-        <span>${item.inCart}</span>
-        </div>
-        <div class="total">
-           Total: <span> ${item.inCart * item.price}</span>
-        </div>
-        </br></br>
-        `
-        });
+        for (let i = 0; i < productInCart.length; i++) {
+            cartContent.insertAdjacentHTML('beforeend', `
+            <div class="    ">
+                <img src="" alt="">
+                    Name Product:  <span>${productInCart[i].productName}</span>
+            </div>
+            <div class="productPrice">
+                Price: <span> ${productInCart[i].price}</span>
+            </div>
+            <div claas="productQuantity">
+                Product Quantity:
+            <span>${productInCart[i].inCart}</span>
+            </div>
+            <div class="total">
+                Total: <span> ${productInCart[i].inCart * productInCart[i].price}</span>
+            </div>
+                <button class="btnRemove">Remove</button>
+            </br></br>`);
+        };
         cartContent.innerHTML += `
         <div class ="totalCart">
             Total Bill: ${cartTotal}
         </div>`
-    }
-    hiddenDivUserInfor()
+
+    };
+
+    const btnRemove = document.getElementsByClassName('btnRemove');
+    for (let i = 0; i < btnRemove.length; i++) {
+        btnRemove[i].addEventListener('click', () => {
+            removeShowProductInCart(i);
+            updateCartTotal(productInCart[i]);
+            updateCartNumber(productInCart[i]);
+            loadListCartProduct();
+        });
+    };
+
     const btnPayment = document.getElementById('btnPayment');
     btnPayment.addEventListener('click', () => {
         showDivUserInfor();
-    })
-
-
+    });
+    hiddenDivUserInfor()
 }
+//function này dùng để xóa sản phẩm bị remove khỏi màn hình và localstorage
+function removeShowProductInCart(chosenProduct) {
+    let productInCart = localStorage.getItem('productInCart');
+    let cartContent = document.getElementById('content');
+    cartContent.innerHTML = '';
+    productInCart = JSON.parse(productInCart);
+    productInCart = (Object.values(productInCart));
+    productInCart.splice(chosenProduct, 1);
+    console.log(productInCart);
+    localStorage.setItem('productInCart', JSON.stringify(productInCart));
+};
+
+//function này dùng để update total cart sau khi remove.
+function updateCartTotal(chosenProduct) {
+    let cartTotal = localStorage.getItem('totalCart');
+    if (cartTotal == 0) {
+        localStorage.setItem('totalCart', cartTotal);
+    } else {
+        localStorage.setItem('totalCart', cartTotal - (chosenProduct.price * chosenProduct.inCart));
+    }
+};
+
+//function này dùng để update số sản phẩm trong cart sau khi remove.
+function updateCartNumber(chosenProduct) {
+    let productNumberInCarts = localStorage.getItem('CartNumbers'); //lấy ra số product đag có trong cart
+    productNumberInCarts = parseInt(productNumberInCarts);
+    if (productNumberInCarts == 0) {
+        localStorage.setItem('CartNumbers', 0);
+        document.getElementById('cartNumbers').textContent = 0;
+    } else {
+        localStorage.setItem('CartNumbers', productNumberInCarts - chosenProduct.inCart);
+        document.getElementById('cartNumbers').textContent = productNumberInCarts - chosenProduct.inCart;
+    }
+}
+
 // function này để ẩn form nhập thông tin giao hàng
 function hiddenDivUserInfor() {
     const userInfor = document.getElementById('userInfor');
     userInfor.style.display = 'none';
 }
-function hiddenDivUserEnter() {
-    const userInfor = document.getElementById('userInfor');
-    userInfor.style.display = 'none';
-}
-//function này dùng để hiện hiện form nhập thông tin và ẩn luôn nút payment
+
+//function này dùgn để hiện hiện form nhập thông tin và ẩn luôn nút payment
 function showDivUserInfor() {
     payment.style.display = 'none';
     const userInfor = document.getElementById('userInfor');
     userInfor.style.display = 'block';
     const btnBuy = document.getElementById('btnBuy');
     btnBuy.addEventListener('click', () => {
-
-        createBill();
-    })
+        creatBill();
+        showBillDetail();
+    });
 }
-//function này tạo bill và lưu vào local, việc lưu vào local giúp sau này lấy lại đc thông tin từ bill chuyển đến mangae bill của admin
-function createBill() {
-    let inName = document.getElementById('inName');
-    let inPhone = document.getElementById('inPhone');
-    let inAdd = document.getElementById('inAdd');
-    let cartItems = localStorage.getItem('productInCart');
-    cartItems = JSON.parse(cartItems);
-    cartItems = (Object.values(cartItems));
+
+//function này tạo bill và lưu vào local, việc lưu vào mocal giúp sau này lấy lại đc thông tin từ bill chuyển đến mangae bill của admin
+function creatBill() {
+    const inName = document.getElementById('inName');
+    const inPhone = document.getElementById('inPhone');
+    const inAddress = document.getElementById('inAdd');
+    const cartTotal = localStorage.getItem('totalCart');
+    let productInCart = localStorage.getItem('productInCart');
+    productInCart = JSON.parse(productInCart);
+    productInCart = (Object.values(productInCart));
+    const newBill = {
+        billID: 1,
+        add: inAddress.value,
+        name: inName.value,
+        phone: inPhone.value,
+        product: productInCart,
+        total: cartTotal.value,
+    }
+
+    const billFormLocalStorage = localStorage.getItem('bill');
+    const billParseJSon = JSON.parse(billFormLocalStorage);
+    let finalBill = newBill;
+    if(billFormLocalStorage){
+        finalBill = {
+            ...billParseJSon,
+            ...newBill
+        }
+        return;
+    }
+
+    localStorage.setItem('bill', JSON.stringify(finalBill));
+    localStorage.removeItem('productInCart');
+    localStorage.removeItem('CartNumbers');
+    localStorage.removeItem('totalCart');
+};
+
+//function này dùng để show ra detail  đơn hàng sau khi ấn buy
+function showBillDetail() {
     let bill = localStorage.getItem('bill');
     bill = JSON.parse(bill);
-    if (bill = null) {
-        bill = {
-            
-            product: cartItems,
-            name: inName.value,
-            phone: inPhone.value,
-            add: inAdd.value
-        }
-    } else {        
-        bill = {
-            ...bill,
-            product: cartItems,
-            name: inName.value,
-            phone: inPhone.value,
-            add: inAdd.value
-        };        
+    //bill = (Object.values(bill));
+    if (confirm(`Your Total Bill: ${bill.total}`) == true) {
+        alert('Buy Success! Please wait for a confirmation from the staff.');
+    } else {
+        alert('Buy Fail.')
     };
-    localStorage.setItem('bill', JSON.stringify(bill));
-    const btnBuy = document.getElementById('btnBuy');
-    const orderBill = document.getElementById('orderBill');
-    btnBuy.addEventListener('click',()=>{
-        // Hiển thị form orderbill thông báo cho người dùng mua hàng thành công (in ra thông tin hóa đơn)
-    })
-}
-
-loadNumberInCart();
+};
 loadListCartProduct();
+loadNumberInCart();
